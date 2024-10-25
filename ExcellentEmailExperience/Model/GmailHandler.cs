f8f6ddf1f8db7ccfcd6d50df58dc0aaa51bdd49b
RancidMarmalade <davidala1email@gmail.com>
2024-10-25 15:37:16 +0200

    sender done og handler start

--- ExcellentEmailExperience/Model/GmailHandler.cs
+++ ExcellentEmailExperience/Model/GmailHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExcellentEmailExperience.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace ExcellentEmailExperience.Model
 {
-    internal class GmailHandler
+    internal class GmailHandler : IMailHandler
     {
+
+
     }
 }
